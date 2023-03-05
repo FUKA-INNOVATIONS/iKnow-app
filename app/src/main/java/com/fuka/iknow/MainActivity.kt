baa10ed5052a43f07b17968b7bddc8b7a1fb27e5
juuso <juusohyv@metropolia.fi>
2023-03-05 13:32:54 +0200

    Added biometric function to app

--- app/src/main/java/com/fuka/iknow/MainActivity.kt
+++ app/src/main/java/com/fuka/iknow/MainActivity.kt
@@ -1,24 +1,26 @@
 package com.fuka.iknow
 
+import android.Manifest
+import android.app.KeyguardManager
 import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.fuka.iknow.boradcast.reciever.AirPlaneBroadcastReceiver
 import com.fuka.iknow.navigation.NavigationPage
+import com.fuka.iknow.screens.LoginScreen
 import com.fuka.iknow.ui.theme.IKnowTheme
 import com.fuka.iknow.viewModels.DatabaseViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,14 +31,82 @@ class MainActivity : ComponentActivity() {
     val viewModel: DatabaseViewModel by viewModels()
 
     private lateinit var br: BroadcastReceiver
+    private var cancellationSignal: CancellationSignal? = null
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContent {
-            NavigationPage(viewModel)
+            IKnowTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    LoginScreen(onClick = { launchBiometric() })
+                }
+            }
         }
+    }
+
+    private val authenticationCallback: android.hardware.biometrics.BiometricPrompt.AuthenticationCallback =
+        @RequiresApi(Build.VERSION_CODES.P)
+        object : android.hardware.biometrics.BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: android.hardware.biometrics.BiometricPrompt.AuthenticationResult?) {
+                super.onAuthenticationSucceeded(result)
+                Toast.makeText(this@MainActivity, "Authentication Succeeded", Toast.LENGTH_SHORT).show()
+                setContent {
+                    NavigationPage(viewModel)
+                }
+            }
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(this@MainActivity, "Authentication Error code: $errorCode", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+            }
+
+            override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
+                super.onAuthenticationHelp(helpCode, helpString)
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun checkBiometricSupport(): Boolean {
+        val keyGuardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        if (!keyGuardManager.isDeviceSecure) {
+            return true
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED) {
+            return false
+        }
+
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun launchBiometric() {
+        if (checkBiometricSupport()) {
+            val biometricPrompt = android.hardware.biometrics.BiometricPrompt.Builder(this)
+                .apply {
+                    setTitle("Biometric login for iKnow")
+                    setSubtitle("Authenticate with your fingerprint")
+                    setConfirmationRequired(false)
+                    setNegativeButton("Cancel", mainExecutor, { _, _, ->
+                        Toast.makeText(this@MainActivity, "Authentication Cancelled", Toast.LENGTH_SHORT).show()
+                    })
+                }.build()
+            biometricPrompt.authenticate(getCancellationSignal(), mainExecutor, authenticationCallback)
+        }
+    }
+
+    private fun getCancellationSignal(): CancellationSignal {
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            Toast.makeText(this, "Authentication Cancelled Signal", Toast.LENGTH_SHORT).show()
+        }
+
+        return cancellationSignal as CancellationSignal
     }
 
     override fun onResume() {
