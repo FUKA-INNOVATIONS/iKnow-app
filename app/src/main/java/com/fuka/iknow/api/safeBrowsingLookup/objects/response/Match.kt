e64969859872fd3d740c79a49de4abf5373dba65
Fuwad Kalhori <35812338+FUKA-INNOVATIONS@users.noreply.github.com>
2023-03-06 17:46:04 +0200

    fixed response

--- app/src/main/java/com/fuka/iknow/api/safeBrowsingLookup/objects/response/Match.kt
+++ app/src/main/java/com/fuka/iknow/api/safeBrowsingLookup/objects/response/Match.kt
@@ -1,14 +1,16 @@
 package com.fuka.iknow.api.safeBrowsingLookup.objects.response
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fuka.iknow.api.safeBrowsingLookup.objects.request.Client
 import java.net.URL
 import java.time.Duration
 
 data class Match(
-  @JsonProperty("threatType") val threatType: String,
-  @JsonProperty("platformType") val platformType: String,
-  @JsonProperty("threatEntryType") val threatEntryType: String,
-  @JsonProperty("threat") val threat: URL,
-  @JsonProperty("threatEntryMetadata") val threatEntryMetadata: List<Entries> = listOf(),
-  @JsonProperty("cacheDuration") val cacheDuration: Duration,
+  @JsonProperty("client") val client: Client?,
+  @JsonProperty("threatType") val threatType: String?,
+  @JsonProperty("platformType") val platformType: String?,
+  @JsonProperty("threat") val threat: Threat?,
+  @JsonProperty("cacheDuration") val cacheDuration: String?,
+  @JsonProperty("threatEntryType") val threatEntryType: String?,
+  @JsonProperty("threatEntryMetadata") val threatEntryMetadata: List<Entries>? = listOf(),
 )
